1d571e98d2c86a33ad050948adc6c2852d5ae1be
GameJam Account045 <gamejam045@saskpolytech.ca>
2025-09-27 17:06:41 -0600

    Added button sounds 2

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+@onready var button = $click
+
+func _on_play_pressed() -> void:
+	button.play()
+	get_tree().change_scene_to_file("res://main.tscn")#change scene to host scene
+
+func _on_quit_pressed() -> void:
+	button.play()
+	get_tree().quit()
--- scenes/ui/play_button.gd
+++ scenes/ui/play_button.gd
@@ -1,5 +1,4 @@
 extends Button
 
-
 func _on_pressed() -> void:
 	pass # Put the game creation scene here ex. get_tree().change_scene_to_file(path to scene here)
