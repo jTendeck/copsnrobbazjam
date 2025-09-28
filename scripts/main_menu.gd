71495c2e351664259960629fd2ee0dfda31ca366
Jonah Konecnik <jkonecnik44@gmail.com>
2025-09-28 00:59:49 -0600

    fixed button sound issue for main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,16 +2,32 @@ extends Node2D
 
 @onready var buttonSound = $Click
 
-@onready var lobby_menu = load("res://scenes/ui/lobby.tscn")
+@onready var lobby_menu = preload("res://scenes/ui/lobby.tscn")
 
 func _ready() -> void:
 	pass
 
 
-func _on_quit_button_button_up() -> void:
+
+func _on_quit_button_button_down() -> void:
+	
 	buttonSound.play()
+	
+	
 
 
+func _on_play_button_button_down() -> void:
+	
+	buttonSound.play()
+	
+	
 func _on_play_button_button_up() -> void:
-	buttonSound.play()
-	lobby_menu.instantiate()
+	
+	await buttonSound.finished
+	get_tree().change_scene_to_packed(lobby_menu)
+
+
+func _on_quit_button_button_up() -> void:
+	await buttonSound.finished
+	
+	get_tree().quit()
